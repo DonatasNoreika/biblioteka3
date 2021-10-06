284be51e0d3789985a3fba8c56f70f4f872617e0
Donoras <donoras@gmail.com>
2021-10-06 20:51:06 +0300

    search

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def index(request):
     # Suskaičiuokime keletą pagrindinių objektų
@@ -52,3 +53,15 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'book_detail.html'
+
+
+def search(request):
+    """
+    paprasta paieška. query ima informaciją iš paieškos laukelio,
+    search_results prafiltruoja pagal įvestą tekstą knygų pavadinimus ir aprašymus.
+    Icontains nuo contains skiriasi tuo, kad icontains ignoruoja ar raidės
+    didžiosios/mažosios.
+    """
+    query = request.GET.get('query')
+    search_results = Book.objects.filter(Q(title__icontains=query) | Q(summary__icontains=query))
+    return render(request, 'search.html', {'books': search_results, 'query': query})
