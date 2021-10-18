6c1940cbc9255493a22dfaf36d67e6b7970afba1
Donoras <donoras@gmail.com>
2021-10-18 19:57:27 +0300

    11 vertimai done. Visų nedariau, nes tingiu :)

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('authors/', views.authors, name="authors"),
     path('authors/<int:author_id>', views.author, name='author'),
     path('books/', views.BookListView.as_view(), name='books'),
